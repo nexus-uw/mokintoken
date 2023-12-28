e7d822c7885fb3cfc8c302179438311892764dbb
Simon Ramsay <nexus-uw@users.noreply.github.com>
2023-12-28 21:35:55 +0000

    progress. still doesnt run

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.21.3
 
-require github.com/mattn/go-sqlite3 v1.14.17 // indirect
+require (
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+)
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -19,12 +21,51 @@ type Note struct {
 	viewCount     int
 }
 
-func lookUpNote(id string) (Note, error) {
+func lookUpNote(db *sql.DB, id string) (Note) {
+	row := db.QueryRow("SELECT encryptedText, expiry, expiresViews, viewCount  FROM notes WHERE id = ? LIMIT 1", id)
+	// todo cleanly account for NOT FOUND
+	var (
+	encryptedText string
+	expiry        string
+	expiresViews  int
+	viewCount     int
+	)
+	err := row.Scan(&encryptedText, &expiry, &expiresViews, &viewCount )
 
+	if err != nil{
+		panic(err)
+	}
+
+	return Note{id, encryptedText, expiry, expiresViews, viewCount}
+	
 }
 
-func saveNote(encryptedText string, expiry string, expiresViews int) string {
+func incrementViews(db *sql.DB, id string)  {
+	_, err:= db.Exec(" TODO id = ?", id)
+	
+	if err != nil{
+		panic(err)
+	}
+}
 
+func deleteNote(db *sql.DB, id string)  {
+	_, err:= db.Exec("DELETE FROM notes WHERE id = ?", id)
+	
+	if err != nil{
+		panic(err)
+	}
+}
+
+func saveNote(db *sql.DB,encryptedText string, expiry string, expiresViews int) string {
+	var id = uuid.New().String()
+
+	_, err:= db.Exec("INSERT INTO notes (id, encryptedText, expiry, expiresViews, viewCount) VALUES (?,?, ?,?,?)", id,encryptedText, expiry, expiresViews, 0)
+	
+	if err != nil{
+		panic(err)
+	}
+
+	return id
 }
 
 func ass(name string) func(w http.ResponseWriter, r *http.Request) {
@@ -52,51 +93,68 @@ func ass(name string) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func decryptHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Path[1:]
-	// db lookup id
-	note, err := lookUpNote(id)
+func decryptHandler(db *sql.DB) func (w http.ResponseWriter, r *http.Request){
+	return func (w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Path[1:]
+		// db lookup id
+		note/*, err*/ := lookUpNote(db, id)
 
-	if err != nil {
-		// todo use nice template
-		http.Error(w, "note not found", http.StatusNotFound)
-		return
-	}
+		/*if err != nil {
+			// todo use nice template
+			http.Error(w, "note not found", http.StatusNotFound)
+			return
+		}*/
+		if (note.expiresViews <= note.viewCount || note.expiry <= fmt.Sprint(time.Now().UTC().UnixMilli()) ){
+			http.Error(w, "note not found", http.StatusNotFound)
+			return // todo nice template
+		}
+		// todo: inspect view count + expiry time
+		// if expired, delete note + return 404
+		// else inc view count
+		incrementViews(db, note.id)
 
-	// Use the template.ParseFiles() function to read the files and store the
-	// templates in a template set. Notice that we use ... to pass the contents
-	// of the files slice as variadic arguments.
-	ts, err := template.ParseFiles(
-		"./views/template.html",
-		"./views/decrypt.html",
-	)
-	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+		// Use the template.ParseFiles() function to read the files and store the
+		// templates in a template set. Notice that we use ... to pass the contents
+		// of the files slice as variadic arguments.
+		ts, err := template.ParseFiles(
+			"./views/template.html",
+			"./views/decrypt.html",
+		)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
-	err = ts.ExecuteTemplate(w, "base", note.encryptedText)
-	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		// Use the ExecuteTemplate() method to write the content of the "base"
+		// template as the response body.
+		err = ts.ExecuteTemplate(w, "base", note.encryptedText)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
 	}
 }
 
-func saveNoteHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "/api/save-note Hi there, I love %s!", r.URL.Path[1:])
+func saveNoteHandler(db *sql.DB) func (w http.ResponseWriter, r *http.Request) {
+	return  func (w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "/api/save-note Hi there, I love %s!", r.URL.Path[1:])
+	 // saveNote
+	}
 }
 
 func main() {
 
 	db, err := sql.Open("sqlite3", "./db.sqlite")
 
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", ass("home"))
 	http.HandleFunc("/about", ass("about"))
-	http.HandleFunc("/decrypt/", decryptHandler)
-	http.HandleFunc("/api/save-note", saveNoteHandler)
+	http.HandleFunc("/decrypt/", decryptHandler(db))
+	http.HandleFunc("/api/save-note", saveNoteHandler(db))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
 	port := os.Getenv("PORT")
