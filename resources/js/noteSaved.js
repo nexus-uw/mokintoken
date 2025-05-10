1ab644434ab15fc6b7a0881638c107d65aed88dd
Simon Ramsay <nexus-uw@users.noreply.github.com>
2025-05-09 20:12:59 -0700

    Update noteSaved.js - all params from hash

--- resources/js/noteSaved.js
+++ resources/js/noteSaved.js
@@ -4,8 +4,11 @@ import QRCode from 'qrcode'
 export class NoteSaved {
 
   constructor() {
-    const hash = window.location.hash.slice(1)
-    const id = new URLSearchParams(window.location.search).get('id')
+
+    const ff = new URLSearchParams(window.location.hash.slice(1))
+    const hash = ff.get('hash')
+    const id = ff.get('id')
+    
 
     const clearnetShareLink = `${document.querySelector('meta[name="clearnet"]').getAttribute('content')}/decrypt/${id}#${hash}`
     document.getElementById('clearnetShareLink').href = clearnetShareLink
